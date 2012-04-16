1f0732539918e73b70ed95936327e1140c5e38dd
Phoenix Information <phoenixinfo2000@gmail.com>
2012-04-16 09:27:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'b70083205510f8b7da4b685714a48ce742bd6974434ad5027a055732db8b3dd2319be42e9f3c8f7327cc8fad39ec917a28c40da1385d8219d7f1828bec68e856'
